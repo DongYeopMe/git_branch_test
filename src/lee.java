7b697d8777f9e6111fecb512d9b59d16cf013bbc
Heoyungyoung <gjdbs2597@naver.com>
2023-04-12 23:40:09 +0900

    nnnndddd

--- src/lee.java
+++ src/lee.java
@@ -7,4 +7,6 @@ public class lee {
     public static void main(String[] args) {
         String str = "우리집 정수기는 얼음나오는 정수기";
     }
+    asmdhasljdhaulrqhwrnqmrnmqnmf;calnc
+    qwe,lwq;me;lqrmq;mr
 }
